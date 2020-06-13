3517e060beec3b806182fba16641c4e42a19a140
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-06-13 02:58:09 -0500

    Added my first api call using sqlite3 that returns all posts from the database

--- posts/post-router.js
+++ posts/post-router.js
@@ -1,12 +1,20 @@
 const express = require('express');
-
+// install knex, sqlite3
 // database access using knex
-const db = require('../data/db-config.js');
+const db = require('../data/db-config.js');//connection to the DB
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    //get the data from the database
+    //send it back
+    db.select('*').from('posts') // returns a promise
+    .then( rows => {
+        res.status(200).json( { data: rows });
+    })
+    .catch(error => {
+        res.status(500).json( { message: 'Sorry, ran into an error'});
+    }); 
 });
 
 router.get('/:id', (req, res) => {
