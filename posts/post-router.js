c230d43a7b6140da5665c84d82869a497aeddfb4
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-06-13 03:27:47 -0500

    Added a post

--- posts/post-router.js
+++ posts/post-router.js
@@ -18,11 +18,32 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
+    db('posts')
+    // .where( "id", "=", req.params.id)
+    .where( { id: req.params.id })
+    .first()// grabs first elemento of the array 
+    .then( post => {
+        if(post) {
+        res.status(201).json( { data: post });
+        } else {
+            res.status(404).json( { message: "nothing found"});
+        }
+    })
+    .catch( error => {
+        res.status(500).json( { message: error });
+    });
 
 });
 
 router.post('/', (req, res) => {
-
+    db('posts')
+    .insert(req.body, 'id')
+    .then(ids => {
+        res.status(201).json( { results: ids });
+    })
+    .catch( error => {
+        res.status(500).json( { message: "error"});
+    });
 });
 
 router.put('/:id', (req, res) => {
